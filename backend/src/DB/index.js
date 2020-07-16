f5384dfe95dbfb3dc700263c9cc7421a75867b9a
Daniel <daniel.snchz.develop@outlook.com>
2020-07-15 19:37:34 -0500

    muchos cambios

--- backend/src/DB/index.js
+++
@@ -1,9 +0,0 @@
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "ieuser",
-  password: "s3kreee7",
-  database: "users"
-});
-
-module.exports = connection;
--- backend/src/public/signup/signupie.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const IEform = document.getElementById('IEform');
-    console.log(IEform[0].value);
-
-})
